013c357e73db4a279fb65797659802ebf7996ae8
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-22 16:00:56 -0700

    added styling to session form

--- frontend/src/components/session/session_form.jsx
+++ frontend/src/components/session/session_form.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../reset.css';
+import './session_form.css';
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -31,19 +33,19 @@ class SessionForm extends React.Component {
     const { errors, formType } = this.props;
     const isSignup = formType === "signup";
     const header = (
-      <div className="header">
+      <div className="session-form-header">
         <h2>
         {isSignup ? /* header text */
           "Sign Up for Whelp"
         : "Log In to Whelp"
         }</h2>
-        <p className="subheading">
+        <p className="session-form-subheading">
         {isSignup ? /* subhead text */
           "Connect with great local businesses"
         : "New to Whelp?"
         }{isSignup ? null /* button display */
         : <Link to='/signup'>Sign Up</Link>}</p>
-        <p className="legal-copy">
+        <p className="session-form-legal-copy">
         {isSignup ? /* legal copy text */
           "By continuing, you agree to Whelp’s Terms of Service and acknowledge Whelp’s Privacy Policy."
         : "By logging in, you agree to Whelp's Terms of Service and Privacy Policy."
@@ -63,61 +65,70 @@ class SessionForm extends React.Component {
     )
 
     return (
-      <div className="session-form-container">
-        {errorsList}
-        {header}
-        <form onSubmit={this.handleSubmit}>
-          {isSignup ? /* first name field display */
-            <input 
-              type="text" 
-              value={this.state.firstName}
-              onChange={this.update('firstName')}
-              placeholder='First Name'
+      <div className="session-form-content-container">
+      {errorsList}
+        <div className="session-form-container">
+          <form id={isSignup ? '' : 'login'} className="session-form" onSubmit={this.handleSubmit}>
+            {header}
+            {isSignup ? (
+              <div className="session-form-name-container">
+                <input /* first name field display */
+                  type="text" 
+                  value={this.state.firstName}
+                  onChange={this.update('firstName')}
+                  placeholder='First Name'
+                  required
+                />
+                <input /* last name field display */
+                type="text"
+                value={this.state.lastName}
+                onChange={this.update('lastName')}
+                placeholder='Last Name'
+                required
+              />
+              </div>
+            ) : null}
+            <input /* email field */
+              type="email"
+              value={this.state.email}
+              onChange={this.update('email')}
+              placeholder='Email'
               required
-            /> : null}
-          {isSignup ? /* last name field display */
-            <input
-              type="text"
-              value={this.state.lastName}
-              onChange={this.update('lastName')}
-              placeholder='Last Name'
+            />
+            <input /* password field */
+              type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+              placeholder='Password'
               required
-            /> : null}
-          <input /* email field */
-            type="email"
-            value={this.state.email}
-            onChange={this.update('email')}
-            placeholder='Email'
-            required
-          />
-          <input /* password field */
-            type="password"
-            value={this.state.password}
-            onChange={this.update('password')}
-            placeholder='Password'
-            required
-          /> {/* add password strength bar later */}
-          {isSignup ? /* zipcode field display */
-            <input 
-              type="text"
-              value={this.state.zipCode}
-              onChange={this.update('zipCode')}
-              placeholder='ZIP Code'
-              required
-            /> : null}
-          <input /* submit button */
-            type="submit"
-            value={isSignup ? /* submit button message */
-              "Sign Up" : "Log In"}
-          />
-        </form>
-        <div className="session-form-subtext-container"> {/* display other choice at bottom of form */}
-          <small className="session-form-subtle-text">
-            {isSignup ? "Already on Whelp?" : "New to Whelp?"}
-            <Link to={`/${isSignup ? 'login' : 'signup'}`}>
-              {isSignup ? "Log In" : "Sign Up"} 
-            </Link>
-          </small>
+            /> {/* add password strength bar later */}
+            {isSignup ? /* zipcode field display */
+              <input 
+                type="text"
+                value={this.state.zipCode}
+                onChange={this.update('zipCode')}
+                placeholder='ZIP Code'
+                id="session-form-zip-code"
+                required
+              /> : null}
+            <input /* submit button */
+              type="submit"
+              value={isSignup ? /* submit button message */
+                "Sign Up" : "Log In"}
+              className="session-form-submit-button"
+            />
+            <div className="session-form-subtext-container"> {/* display other choice at bottom of form */}
+              <small className="session-form-subtle-text">
+                {isSignup ? "Already on Whelp?" : "New to Whelp?"}
+                <Link to={`/${isSignup ? 'login' : 'signup'}`}>
+                  {isSignup ? "Log In" : "Sign Up"} 
+                </Link>
+              </small>
+            </div>
+          </form>
+          <div className="session-form-picture-container">
+            <img src="https://s3-media4.fl.yelpcdn.com/assets/2/www/img/7922e77f338d/signup/signup_illustration.png" alt=""/>
+          </div>
         </div>
       </div>
     )
