a1dd955ab13648d9383c07838bed5ba8cbf3a57c
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-22 11:56:39 -0400

    Complete frontend user auth

---
+++ frontend/src/actions/session_actions.js
@@ -0,0 +1,52 @@
+import * as APIUtil from '../util/session_api_util';
+import jwt_decode from 'jwt-decode';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user)
+    .then(res => {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        APIUtil.setAuthToken(token);
+        const decodedUser = jwt_decode(token);
+        dispatch(receiveCurrentUser(decodedUser));
+      },
+      err => dispatch(receiveErrors(err.response.data))
+    )
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      APIUtil.setAuthToken(token);
+      const decodedUser = jwt_decode(token);
+      dispatch(receiveCurrentUser(decodedUser));
+    },
+      err => dispatch(receiveErrors(err.response.data))
+    )
+);
+
+export const logout = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  APIUtil.setAuthToken(false);
+  dispatch(logoutUser());
+};
---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+const App = () => (
+  <Switch>
+
+  </Switch>
+);
+
+export default App;
