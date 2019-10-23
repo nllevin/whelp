2805ece0b542af389e4b4b530de685bc4cfb3c82
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-22 22:51:13 -0400

    Import HeaderNav into App

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
+import HeaderNav from './header_nav/header_nav';
 
 const App = () => (
   <Switch>
+    <HeaderNav />
 
   </Switch>
 );
