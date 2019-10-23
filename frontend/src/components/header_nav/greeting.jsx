f39dbfa229917b2d10b6de2d27fa801bfbe4c684
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-22 22:56:12 -0400

    Add and style Greeting component

---
+++ frontend/src/components/header_nav/greeting.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProfileDropdown from "./profile_dropdown";
+import './greeting.css';
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { dropdownOpen: false };
+    this.closeDropdown = this.closeDropdown.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  closeDropdown() {
+    this.setState({ dropdownOpen: false });
+  }
+
+  toggleDropdown(e) {
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
+  }
+
+  render() {
+    const { currentUser, logout } = this.props;
+
+    const loggedOutGreeting = (
+      <div className="greeting-container">
+        <Link to="/login" className="session-button">Log In</Link>
+        <Link to="/signup" className="session-button">Sign Up</Link>
+      </div>
+    );
+
+    const loggedInGreeting = (
+      <div className="greeting-container">
+        <button className="profile-dropdown-button" onClick={this.toggleDropdown}>
+          <i className="profile-avatar"></i>
+          <span className="profile-down-arrow"></span>
+        </button>
+        {
+          this.state.dropdownOpen ? 
+          <ProfileDropdown
+            currentUser={currentUser}
+            logout={logout}
+            toggleDropdown={this.toggleDropdown}
+            closeDropdown={this.closeDropdown}
+          /> : ""
+        }
+      </div>
+    );
+
+    return currentUser ? loggedInGreeting : loggedOutGreeting;
+  }
+}
+
+export default Greeting;
