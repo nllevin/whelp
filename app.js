95d3d21bb2bd6b8a313be75ac50224ac90dd6124
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-24 10:21:08 -0400

    Add business#index, #show routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
+const users = require("./routes/api/businesses");
 const bodyParser = require('body-parser');
 const passport = require("passport");
 const path = require('path');
@@ -27,6 +28,7 @@ mongoose
 app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/api/users", users);
+app.use("/api/businesses", businesses);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ routes/api/businesses.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Business = require('../../models/Business');
+const Review = require('../../models/Review');
+
+// business#index
+// will be changed later to incorporate search/filters
+// grabbing all businesses for now
+router.get('/', (req, res) => {
+  Business
+    .find()
+    .then(businessesArray => {
+      const businesses = {};
+      businessesArray.forEach(business => {
+        businesses[business.id] = business;
+      });
+      res.json(businesses);
+    })
+    .catch(err => res.status(404).json({ nobusinessesfound: "No businesses found" }));
+});
+
+// business#show
+router.get('/:id', (req, res) => {
+  Business
+    .findById(req.params.id)
+    .then(business => {
+      Review
+        .find({businessId: business.id})
+        .then(reviewsArray => {
+          const reviews = {};
+          reviewsArray.forEach(review => {
+            reviews[review.id] = review;
+          });
+          res.json({
+            business,
+            reviews
+          });
+        })
+        .catch(err => res.status(404).json({ noreviewsfound: "No reviews found for that business"})); // This doesn't seem right. Some businesses won't have reviews
+    })
+    .catch(err => res.status(404).json({ nobusinessfound: "No business found with that ID" }));
+});
