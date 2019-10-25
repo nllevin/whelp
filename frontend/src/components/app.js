baea4ec38766b0566ac0e952a190e90f8104caf1
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-25 12:33:26 -0400

    Add and style Footer component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,13 +5,17 @@ import { Switch, Route } from 'react-router-dom';
 import LoginForm from './session/login_container';
 import SignupForm from './session/signup_container';
 import Splash from './splash/splash';
+import Footer from './footer/footer';
 
 const App = () => (
-  <Switch>
-    <AuthRoute path='/login' component={LoginForm} />
-    <AuthRoute path='/signup' component={SignupForm} />
-    <Route path='/splash' component={Splash} />
-  </Switch>
+  <div>
+    <Switch>
+      <AuthRoute path='/login' component={LoginForm} />
+      <AuthRoute path='/signup' component={SignupForm} />
+      <Route path='/splash' component={Splash} />
+    </Switch>
+    <Footer />
+  </div>
 );
 
 export default App;
