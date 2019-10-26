957bc0304585085b35ac062149caa3c9f567cdca
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-26 18:09:05 -0400

    Add and style display for no search results

--- frontend/src/components/business_index/business_index.jsx
+++ frontend/src/components/business_index/business_index.jsx
@@ -18,26 +18,37 @@ class BusinessIndex extends React.Component {
 
   render() {
     const { businesses } = this.props;
-    if (businesses.length === 0) return null;
+    const noResultsDisplay = (
+        <div className="business-index-content no-search-results">
+          <h4>Suggestions for improving the results:</h4>
+          <p>Try a different location.</p>
+          <p>Check the spelling or try alternate spellings.</p>
+          <p>Try a more general search, e.g. "pizza" instead of "pepperoni".</p>
+        </div>
+    );
+
+    const resultsDisplay = (
+      <main className="business-index-content">
+        <h2>All Results</h2>
+        <ul>
+          {
+            businesses.map((business, idx) => (
+              <BusinessIndexItem
+                key={business._id}
+                idx={idx}
+                business={business}
+              />
+            ))
+          }
+        </ul>
+      </main>
+    );
 
     return (
       <div className="business-index-container">
         <HeaderNav />
         <div className="business-index-content-container">
-          <main className="business-index-content">
-            <h2>All Results</h2>
-            <ul>
-              {
-                businesses.map((business, idx) => (
-                  <BusinessIndexItem
-                    key={business._id}
-                    idx={idx}
-                    business={business}
-                  />
-                ))
-              }
-            </ul>
-          </main>
+          {businesses.length === 0 ? noResultsDisplay : resultsDisplay}
           <aside className="business-index-sidebar">
             <span>map and ads go here</span>
           </aside>
