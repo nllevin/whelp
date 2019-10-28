c01ca26f1b91711cb2c5e44357f18cf610cbbac0
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-28 15:52:16 -0400

    Add logic to load Google script, render basic map

--- frontend/src/components/business_index/search_map.jsx
+++ frontend/src/components/business_index/search_map.jsx
@@ -1,28 +1,62 @@
 import React from 'react';
+import './search_map.css';
 const APIKey = require('../../config/keys').googleAPI;
 
 class SearchMap extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isMounting: true };
+    if (!window.google) {
+      this.state = { isLoadingScript: true };
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${APIKey}`;
+      script.addEventListener('load', () => this.setState({ 
+        isLoadingScript: false,
+        hasMap: false
+      }));
+      document.head.append(script);
+    } else {
+      this.state = { 
+        isLoadingScript: false,
+        hasMap: false 
+      };
+    }
   }
 
   componentDidMount() {
-    debugger;
+    if (this.state.isLoadingScript) {
+      return;
+    } else {
+      this.constructMap();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasMap) {
+      return;
+    } else {
+      this.constructMap();
+    }
+  }
+  
+  constructMap() {
+    this.setState({ hasMap: true });
     const google = window.google;
     const { lat, lng } = this.props;
     const mapOptions = {
-      center: { lat, lng },
+      center: { 
+        lat: parseInt(lat), 
+        lng: parseInt(lng) 
+      },
       zoom: 13
     };
     this.map = new google.maps.Map(this.mapNode, mapOptions);
   }
 
   render() {
-    debugger;
+    if (this.state.isLoadingScript) return null;
+
     return (
       <div className="search-map-container" ref={map => this.mapNode = map}>
-        <script type="text/javascript" src={`https://maps.googleapis.com/maps/api/js?key=${APIKey}`}></script>
       </div>
     );
   }
