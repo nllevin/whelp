02eb7f443852bfbad857344c957897d8f358d304
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-28 09:26:01 -0700

    added author details to review index

--- frontend/src/components/review_index/review_index_item.jsx
+++ frontend/src/components/review_index/review_index_item.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default ({ review }) => (
+const ReviewItem = ({ review, author }) => (
   <li className="review-index-item">
     <div className="review-index-item-author">
-      <span className="review-index-item-author-name">Placeholder :)</span>
+      <img className="review-author-avatar" src={author.avatarUrl} alt=""></img> 
+      <div className="review-index-author-details">
+        <span className="review-index-item-author-name">{author.firstName} {author.lastName}</span>
+        <span className="review-index-author-zipcode">{author.zipCode}</span>
+      </div>
     </div>
     <div className="review-index-item-content">
       <div className="review-index-item-rating-and-date">
@@ -15,3 +20,12 @@ export default ({ review }) => (
     </div>
   </li>
 )
+
+const mapStateToProps = (state, ownProps) => ({
+  author: state.entities.users[ownProps.review.authorId]
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(ReviewItem);
