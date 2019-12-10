c3a30fd304397070eab87841e541278357f33666
Patrick-Mondala <patrickmondala@gmail.com>
2019-12-09 16:48:11 -0800

    added links to footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -15,6 +15,21 @@ const Footer = () => (
         <a href="https://github.com/nllevin">Noah Levin</a>
         <a href="https://github.com/Patrick-Mondala">Patrick Mondala</a>
       </div>
+      <div className="footer-column">
+        <h3>LinkedIn</h3>
+        <a href="https://www.linkedin.com/in/noah-levin-68b6b4197/">Noah Levin</a>
+        <a href="https://www.linkedin.com/in/patrick-mondala/">Patrick Mondala</a>
+      </div>
+      <div className="footer-column">
+        <h3>Angel List</h3>
+        <a href="https://angel.co/noah-levin-2">Noah Levin</a>
+        <a href="https://angel.co/patrick-mondala">Patrick Mondala</a>
+      </div>
+      <div className="footer-column">
+        <h3>Portfolios</h3>
+        <a href="https://www.nllevin.com/">Noah Levin</a>
+        <a href="https://patrick-mondala.github.io/portfolio-site">Patrick Mondala</a>
+      </div>
     </div>
     <figure className="footer-cityscape" />
   </div>
